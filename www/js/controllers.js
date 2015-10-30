462028856a0a5fb0797a01cadaf4c79b0160394b
jtrussell <jus.russell@gmail.com>
2015-10-30 15:19:16 -0400

    Added ball passing :)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('tock.controllers', ['tock.factories'])
 
-.controller('AppCtrl', function(settings) {
+.controller('AppCtrl', function($scope, settings) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -12,4 +12,22 @@ angular.module('tock.controllers', ['tock.factories'])
 
   self.players = []
   self.players.length = self.settings.playerCount;
+
+  self.isPaused = true;
+
+  $scope.$on('tockerEvent_click', function(event, tIndex, tRunning) {
+    if(tRunning) {
+        $scope.$broadcast('tockerEvent_start', 1 - tIndex);
+        $scope.$broadcast('tockerEvent_pause', tIndex);
+    } else {
+      if(self.isPaused) {
+        $scope.$broadcast('tockerEvent_start', 1 - tIndex);
+        self.isPaused = false;
+      } else {
+        $scope.$broadcast('tockerEvent_pause', 0);
+        $scope.$broadcast('tockerEvent_pause', 1);
+        self.isPaused = true;
+      }
+    }
+  });
 })
--- www/js/directives.js
+++ www/js/directives.js
@@ -4,6 +4,43 @@ angular.module('tock.directives', ['tock.factories'])
 .directive('tocker', function() {
   return {
     restrict: 'AE',
-    template: 'tocker'
+    scope: {
+      tockerIndex: '=',
+      tockerTime: '='
+    },
+    template: [
+      '<span class="tocker-text">',
+        '<span ng-show="tckr.isRunning">running</span>',
+        '<span ng-hide="tckr.isRunning">not running</span>',
+      '</span>'
+    ].join('\n'),
+    link: function(scope, element, attrs) {
+      var index = scope.tockerIndex
+        , timeInitial = scope.tockerTime
+        , timeLeft = timeInitial;
+
+      var t = scope.tckr = {};
+      t.isRunning = false;
+
+      element.on('click', function() {
+        scope.$apply(function() {
+          scope.$emit('tockerEvent_click', index, t.isRunning);
+        });
+      });
+
+      scope.$on('tockerEvent_pause', function(event, tIndex) {
+        if(tIndex === index) {
+          t.isRunning = false;
+          element.removeClass('active');
+        }
+      });
+
+      scope.$on('tockerEvent_start', function(event, tIndex) {
+        if(tIndex === index) {
+          t.isRunning = true;
+          element.addClass('active');
+        }
+      });
+    }
   };
 });
