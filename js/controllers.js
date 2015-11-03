a42458c73e70cdb565ee8f5a9dd6ad910b0dedd4
jtrussell <jus.russell@gmail.com>
2015-11-03 12:54:26 -0500

    Initial commit

---
+++ js/controllers.js
@@ -0,0 +1,33 @@
+angular.module('tock.controllers', ['tock.factories'])
+
+.controller('AppCtrl', function($scope, settings) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  var self = this;
+  self.settings = settings;
+
+  self.players = []
+  self.players.length = self.settings.playerCount;
+
+  self.isPaused = true;
+
+  $scope.$on('tockerEvent_click', function(event, tIndex, tRunning) {
+    if(tRunning) {
+        $scope.$broadcast('tockerEvent_start', 1 - tIndex);
+        $scope.$broadcast('tockerEvent_pause', tIndex);
+    } else {
+      if(self.isPaused) {
+        $scope.$broadcast('tockerEvent_start', 1 - tIndex);
+        self.isPaused = false;
+      } else {
+        $scope.$broadcast('tockerEvent_pause', 0);
+        $scope.$broadcast('tockerEvent_pause', 1);
+        self.isPaused = true;
+      }
+    }
+  });
+})
