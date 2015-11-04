463805e1140d7ac6707e8cf0820b834c14745ab1
jtrussell <jus.russell@gmail.com>
2015-11-04 16:56:07 -0500

    Working.. eh?

--- js/directives.js
+++ js/directives.js
@@ -1,7 +1,7 @@
 
 angular.module('tock.directives', ['tock.factories'])
 
-.directive('tocker', function() {
+.directive('tocker', function($timeout) {
   return {
     restrict: 'AE',
     scope: {
@@ -11,17 +11,54 @@ angular.module('tock.directives', ['tock.factories'])
     template: [
       '<span class="tocker-text">',
         '{{tckr.time | tockTime}}',
-      '</span>'
+      '</span>',
+      '<span class="tocker-progress"></span>'
     ].join('\n'),
     link: function(scope, element, attrs) {
       var index = scope.tockerIndex
-        , timeInitial = scope.tockerTime
-        , timeLeft = timeInitial;
+        , timeInitial
+        , dateLastRun;
 
       var t = scope.tckr = {};
-      t.isRunning = false;
 
-      t.time = timeLeft;
+      var tInit = function() {
+        t.isRunning = false;
+        timeInitial = scope.tockerTime * 60 * 1000
+        t.time = timeInitial;
+      };
+
+      var tStart = function() {
+        t.isRunning = true;
+        element.addClass('active');
+        dateLastRun = dateLastRun || Date.now();
+        tRun();
+      };
+
+      var tStop = function() {
+        t.isRunning = false;
+        element.removeClass('active');
+        dateLastRun = null;
+      };
+
+      var tProg = function() {
+        var $elProg = element.children().eq(1)
+          , prog = Math.floor(100 * t.time / timeInitial);
+        $elProg.css('width', prog + '%');
+      };
+
+      var tRun = function() {
+        if(!t.isRunning) { return; }
+        var now = Date.now();
+        t.time = Math.max(t.time - (now - dateLastRun), 0);
+        dateLastRun = now;
+        tProg();
+        if(!t.time) {
+          element.addClass('dead');
+        }
+        $timeout(tRun, 100);
+      };
+
+      tInit();
 
       element.on('click', function() {
         scope.$apply(function() {
@@ -31,16 +68,19 @@ angular.module('tock.directives', ['tock.factories'])
 
       scope.$on('tockerEvent_pause', function(event, tIndex) {
         if(tIndex === index) {
-          t.isRunning = false;
-          element.removeClass('active');
+          tStop();
         }
       });
 
       scope.$on('tockerEvent_start', function(event, tIndex) {
         if(tIndex === index) {
-          t.isRunning = true;
-          element.addClass('active');
+          tStart();
         }
+      });
+
+      scope.$on('tockerEvent_restart', function(event) {
+        tStop();
+        tInit();
       });
     }
   };
--- js/factories.js
+++ js/factories.js
@@ -3,8 +3,9 @@ angular.module('tock.factories', [])
 
 .factory('settings', function() {
   return {
-    playerCount: 2,
-    playerTime: 12 * 60 * 1000,
-    countDown: true
+    /**
+     * Time alloted per player in minutes
+     */
+    playerTime: 12
   };
 });
