369e73810669f39f6fc6d98c16b1cbc2e9ad0613
jtrussell <jus.russell@gmail.com>
2015-11-06 08:27:11 -0500

    Disable clicks after a timeout until a reset
    Closes #1

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,28 +7,37 @@ angular.module('tock.controllers', ['tock.factories'])
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-  var self = this;
+  var self = this
+    , isPaused = true
+    , isTimedOut = false;
+
   self.settings = settings;
 
-  self.isPaused = true;
-
   self.restart = function() {
+    isTimedOut = false;
     $scope.$broadcast('tockerEvent_restart');
   };
 
   $scope.$on('tockerEvent_click', function(event, tIndex, tRunning) {
+    if(isTimedOut) { return; }
     if(tRunning) {
         $scope.$broadcast('tockerEvent_start', 1 - tIndex);
         $scope.$broadcast('tockerEvent_pause', tIndex);
     } else {
-      if(self.isPaused) {
+      if(isPaused) {
+        isPaused = false;
         $scope.$broadcast('tockerEvent_start', 1 - tIndex);
-        self.isPaused = false;
       } else {
+        isPaused = true;
         $scope.$broadcast('tockerEvent_pause', 0);
         $scope.$broadcast('tockerEvent_pause', 1);
-        self.isPaused = true;
       }
     }
+  });
+
+  $scope.$on('tockerEvent_timeout', function() {
+    isTimedOut = true;
+    $scope.$broadcast('tockerEvent_pause', 0);
+    $scope.$broadcast('tockerEvent_pause', 1);
   });
 })
